beb2f31caa95a28327b11bb9641eddc78cf5c26d
Stanley Huang 黃昭源 <stanley.huang@TPMACA03605.local>
2026-07-03 09:54:30 +0800

    chore: migrate tailwind to v4

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     visualizer({
       open: true,
       filename: 'stats.html',
